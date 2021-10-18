f7a53a21650d9a56b9020269d9cfd49e8eaa78a7
mixa <mihsa11ru@gmail.com>
2021-10-18 16:00:17 +0300

    Initial Commit

---
+++ laba_git_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  laba_git_test
+//
+//  Created by Михаил Козлов on 18.10.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
